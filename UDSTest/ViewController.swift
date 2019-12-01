da73010d47ea8d16df0c5368f41a2bae30290eac
Alan Victor <alanvictor5447@gmail.com>
2019-12-01 11:00:24 -0300

    create project from xcode

---
+++ UDSTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UDSTest
+//
+//  Created by Alan Victor Paulino de Oliveira on 01/12/19.
+//  Copyright © 2019 Alan Victor Paulino de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

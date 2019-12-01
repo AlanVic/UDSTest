707287a9446271bb0f9fda5136b6894db4be78a9
Alan Victor <alanvictor5447@gmail.com>
2019-12-01 17:44:47 -0300

    config the CustomTabBarController

--- UDSTest/App/Coordinator/AppDelegate.swift
+++ UDSTest/App/Coordinator/AppDelegate.swift
@@ -16,8 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
+        let initialViewController = CustomTabBarController()
+        initialViewController.view.backgroundColor = .red
+        
+        window?.rootViewController = initialViewController
         window?.makeKeyAndVisible()
-        window?.rootViewController = ViewController()
         return true
     }
  
--- UDSTest/App/Coordinator/CustomTabBarController.swift
+++ UDSTest/App/Coordinator/CustomTabBarController.swift
@@ -7,13 +7,35 @@
 //
 
 import UIKit
+private protocol ConfigurableTabBar {
+    func setupTabBar()
+    func createNavigation(viewController: UIViewController, title: String, imageNamed: String) -> UINavigationController
+}
 
 class CustomTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        setupTabBar()
+        self.view.backgroundColor = .blue
     }
 
+}
+
+extension CustomTabBarController: ConfigurableTabBar {
+    fileprivate func setupTabBar() {
+        self.viewControllers = [
+            createNavigation(viewController: LoginViewController(), title: "Login", imageNamed: "profile-icon"),
+            createNavigation(viewController: LoginViewController(), title: "Login", imageNamed: "profile-icon")
+        ]
+    }
+    
+    func createNavigation(viewController: UIViewController, title: String, imageNamed: String) -> UINavigationController {
+        let navigation = UINavigationController(rootViewController: viewController)
+        navigation.tabBarItem.image = UIImage(named: imageNamed)
+        navigation.tabBarItem.imageInsets = UIEdgeInsets(top: 5, left: 0, bottom: -5, right: 0)
+        return navigation
+    }
+    
+    
 }

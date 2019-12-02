1ed1ff8e378c7c10f0274437b1f079634c404713
Alan Victor <alanvictor5447@gmail.com>
2019-12-02 14:03:41 -0300

    add googleService from firebase

--- UDSTest/App/Coordinator/AppDelegate.swift
+++ UDSTest/App/Coordinator/AppDelegate.swift
@@ -21,8 +21,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window?.rootViewController = initialViewController
         window?.makeKeyAndVisible()
-        
-        let _ = User
         return true
     }
  

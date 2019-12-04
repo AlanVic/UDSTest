422cd9318a3ed969cb2e5863d539e1cb8dbcd0fb
Alan Victor <alanvictor5447@gmail.com>
2019-12-04 08:46:21 -0300

    make LoginViewModel

--- UDSTest/App/Features/Login/ViewModel/LoginViewModel.swift
+++ UDSTest/App/Features/Login/ViewModel/LoginViewModel.swift
@@ -7,7 +7,21 @@
 //
 
 import Foundation
+import FirebaseAuth
 
 struct LoginViewModel {
+    
+    let firebaseAuth = Auth.auth()
+    
+    func loginAuth(withEmail email: String?, andPassword password: String?, completion: @escaping (Bool) -> Void) {
+        guard let userEmail = email, let userPassword = password else {
+            return completion(false)
+        }
+        
+        firebaseAuth.signIn(withEmail: userEmail, password: userPassword) { (_, error) in
+            if error != nil { return completion(false) }
+            completion(true)
+        }
+    }
     
 }

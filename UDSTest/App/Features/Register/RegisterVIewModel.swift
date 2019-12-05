2d342785168ab06bf785031ffec84555c6d15f7c
Alan Victor <alanvictor5447@gmail.com>
2019-12-05 18:06:59 -0300

    registerViewModel was created

---
+++ UDSTest/App/Features/Register/RegisterVIewModel.swift
@@ -0,0 +1,23 @@
+//
+//  RegisterVIewModel.swift
+//  UDSTest
+//
+//  Created by Alan Victor Paulino de Oliveira on 05/12/19.
+//  Copyright © 2019 Alan Victor Paulino de Oliveira. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+struct RegisterVIewModel {
+    let firebaseAuth = Auth.auth()
+    
+    func signUpUser(withEmail email:String?, andPassword password:String?, completion: @escaping (Bool) -> Void) {
+        guard let userEmail = email, let userPassword = password else { return completion(false) }
+        
+        firebaseAuth.createUser(withEmail: userEmail, password: userPassword) { (_, error) in
+            if error != nil { completion(false) }
+            completion(true)
+        }
+    }
+}

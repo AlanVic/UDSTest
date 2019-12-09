91c427d468859dc0810494c0007f2e11721cc4e3
Alan Victor <alanvictor5447@gmail.com>
2019-12-09 12:44:59 -0300

    update IconsTab

--- UDSTest/App/Coordinator/CustomTabBarController.swift
+++ UDSTest/App/Coordinator/CustomTabBarController.swift
@@ -34,9 +34,9 @@ class CustomTabBarController: UITabBarController {
 extension CustomTabBarController: ConfigurableTabBar {
     fileprivate func setupTabBar() {
         self.viewControllers = [
-            createNavigation(viewController: PautasViewController(withTypeController: .open), title: "Pautas Abertas", imageNamed: "profile-icon"),
-            createNavigation(viewController: PautasViewController(withTypeController: .finished), title: "Pautas Fechadas", imageNamed: "profile-icon"),
-            createNavigation(viewController: SettingsViewController(), title: "Settings", imageNamed: "profile-icon")
+            createNavigation(viewController: PautasViewController(withTypeController: .open), title: "Pautas Abertas", imageNamed: "pauta"),
+            createNavigation(viewController: PautasViewController(withTypeController: .finished), title: "Pautas Fechadas", imageNamed: "pauta"),
+            createNavigation(viewController: SettingsViewController(), title: "Configurações", imageNamed: "settings")
         ]
     }
     
@@ -44,7 +44,7 @@ extension CustomTabBarController: ConfigurableTabBar {
         let navigation = UINavigationController(rootViewController: viewController)
         navigation.title = title
         navigation.tabBarItem.image = UIImage(named: imageNamed)
-        navigation.tabBarItem.imageInsets = UIEdgeInsets(top: 5, left: 0, bottom: -5, right: 0)
+//        navigation.tabBarItem.imageInsets = UIEdgeInsets(top: 5, left: 0, bottom: -5, right: 0)
         return navigation
     }
     
